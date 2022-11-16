941abd48bc53d520f7bad0df4cf5163610827f86
momo8042 <88641414+momo8042@users.noreply.github.com>
2022-11-16 19:30:04 +0800

    Update Internal_courses.py

--- cogs/Internal_courses.py
+++ cogs/Internal_courses.py
@@ -20,17 +20,6 @@ class template_5(discord.ui.Modal, title ='對內課程公告'):
                                                 \n．課程教材:\
                                                 \n\t．{self.course_teachingMaterial}\
                                                 \n(如有要請假的請留言回覆告知)', ephemeral=False)
-        member = await interaction.original_response()
-        mem_data = str(member)
-        result = []
-        id_split = mem_data.split("=")
-        for i in range(len(id_split)):
-            id_split_2 = id_split[i].split(' ')
-            for j in range(len(id_split_2)):
-                if id_split_2[j] == 'channel':
-                    # print (id_split_2[0])
-                    result.append(id_split_2[0])
-        self.final_result = str("".join(result))
 
 class who_is_the_lucky_guy(discord.ui.Modal, title ='This 15 Speech'):
     fifteen_speech = discord.ui.TextInput(label = '幸運兒', placeholder='請填寫帳號全名')
@@ -41,14 +30,9 @@ class who_is_the_lucky_guy(discord.ui.Modal, title ='This 15 Speech'):
                 self.final_member_id = member.id
         await interaction.response.send_message(f'> This 15 Speech: <@{self.final_member_id}>', ephemeral=False)
 
-class Modal_5(commands.Cog, template_5, who_is_the_lucky_guy):
+class Modal_5(commands.Cog, template_5):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if "This 15 Speech" in message.content:
-            self.channel_id = template_5.final_result
-            self.lucky_guy = who_is_the_lucky_guy.final_member_id
 async def setup(bot):
     await bot.add_cog(Modal_5(bot))
