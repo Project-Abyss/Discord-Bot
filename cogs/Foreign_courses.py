51cec460260ca408ecd75cff0eec29fa49bee35c
momo8042 <88641414+momo8042@users.noreply.github.com>
2022-11-19 12:27:33 +0800

    Update Foreign_courses.py

--- cogs/Foreign_courses.py
+++ cogs/Foreign_courses.py
@@ -6,7 +6,7 @@ class template_4(discord.ui.Modal, title ='對外課程公告'):
     course_location = discord.ui.TextInput(label = '地點')
     course_title = discord.ui.TextInput(label = '課程名稱')
     course_lecturer = discord.ui.TextInput(label = '課程講師', placeholder='請填寫帳號全名')
-    course_slides_note = discord.ui.TextInput(label = '簡報筆記 (簡報－ s <url> / 筆記－ n <url>)', style = discord.TextStyle.paragraph, default='s \nn ')
+    course_slides_note = discord.ui.TextInput(label = '簡報筆記 (簡報－ S <url> / 筆記－ N <url>)', style = discord.TextStyle.paragraph, default='S \nN ')
     async def on_submit(self, interaction: discord.Interaction):
         slide_and_note =str(self.course_slides_note)
         result = []
@@ -16,12 +16,12 @@ class template_4(discord.ui.Modal, title ='對外課程公告'):
         for i in range(len(slide_split)):
             slide_split_2 = slide_split[i].split(' ')
             for j in range(len(slide_split_2)):
-                if slide_split_2[0] == 's' and slide_split_2[1] != "":
+                if (slide_split_2[0] == 's' or slide_split_2[0] == 'S') and slide_split_2[1] != "":
                     slide_split_new = str(slide_split_2[1])
                     slide_split_2[j] =  '     ．簡報 －' + slide_split_new +'\n'
                     result.append(slide_split_2[j])
                     s = s + 1
-                elif slide_split_2[0] == 'n' and slide_split_2[1] != "":
+                elif (slide_split_2[0] == 'n' or slide_split_2[0] == 'N')and slide_split_2[1] != "":
                     slide_split_new = str(slide_split_2[1])
                     slide_split_2[j] = '     ．筆記 －' + slide_split_new + '\n'
                     result.append(slide_split_2[j])
