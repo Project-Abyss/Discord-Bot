8018e6abd6cb4d7501d18165b859f6c2c0246ee8
bessyhuang <bessyhuang1@gmail.com>
2023-03-13 22:16:07 +0800

    Fix: Command Output Format

--- cogs/drawlottery.py
+++ cogs/drawlottery.py
@@ -15,7 +15,7 @@ class DrawLottery(commands.Cog):
     async def on_ready(self):
         print('DrawLottery cog loaded.')
 
-    @commands.command(name='MembersInfo', aliases=['info'], brief="\n")
+    @commands.command(name='MembersInfo', aliases=['info'], brief="\n\t查看頻道內的成員。\n\t【 範例：!info 】\n")
     async def members_info(self, ctx):
         real_member_count = 0
         real_member_list = []
@@ -30,9 +30,9 @@ class DrawLottery(commands.Cog):
         self._list_member = real_member_list
 
         await ctx.send(f'There are {self._amount_member} members in this channel ~')
-        await ctx.send(f'Members: {self._list_member}')
+        await ctx.send(f'Members: {" ".join(self._list_member)}')
 
-    @commands.command(name='依據指定成員抽籤', aliases=['member'], brief="\n\t被點到的人都逃不掉！\n\t【 範例：!member_v2 2 成員1 成員2 成員3 】\n")
+    @commands.command(name='依據指定成員抽籤', aliases=['member'], brief="\n\t被點到的人都逃不掉！\n\t【 範例：!member 2 成員1 成員2 成員3 】\n")
     async def draw_specific_lottery_member(self, ctx, amount: int, *users):
         all_members_list = []
         for user in users:
@@ -41,10 +41,10 @@ class DrawLottery(commands.Cog):
             if user_name not in all_members_list:
                 all_members_list.append(user_name.mention)
         
-        await ctx.send(f'參與抽籤的成員：{all_members_list}')
+        await ctx.send(f'參與抽籤的成員：{" ".join(all_members_list)}')
 
         result = random.sample(all_members_list, k=amount)
-        await ctx.send(f'公佈中籤名單：{result}')
+        await ctx.send(f'公佈中籤名單：{" ".join(result)}')
 
     @commands.command(name='依據身份組抽籤', aliases=['role'], brief="\n\t選擇多個身份組後，再抽簽！\n\t【 範例：!role 2 身份組1 身份組2 身份組3 】\n")
     async def draw_lottery_role(self, ctx, amount: int, *roles):
@@ -57,10 +57,10 @@ class DrawLottery(commands.Cog):
                 if m not in all_members_list:
                     all_members_list.append(m.mention)
         
-        await ctx.send(f'參與抽籤的成員：{all_members_list}')
+        await ctx.send(f'參與抽籤的成員：{" ".join(all_members_list)}')
 
         result = random.sample(all_members_list, k=amount)
-        await ctx.send(f'公佈中籤名單：{result}')
+        await ctx.send(f'公佈中籤名單：{" ".join(result)}')
 
     @commands.command(name='依據指定成員或身份組抽籤', aliases=['mix'], brief="\n\t選擇多個指定成員或身份組後，再抽簽！\n\t【 範例：!mix 2 身份組1 成員1 成員2 身份組2 身份組3 成員3 】\n")
     async def draw_lottery_member_and_role(self, ctx, amount: int, *somebody):
@@ -79,10 +79,10 @@ class DrawLottery(commands.Cog):
                 if user_name.mention not in all_members_list:
                     all_members_list.append(user_name.mention)
 
-        await ctx.send(f'參與抽籤的成員：{all_members_list}')
+        await ctx.send(f'參與抽籤的成員：{" ".join(all_members_list)}')
         
         result = random.sample(all_members_list, k=amount)
-        await ctx.send(f'公佈中籤名單：{result}')
+        await ctx.send(f'公佈中籤名單：{" ".join(result)}')
 
 async def setup(bot):
     await bot.add_cog(DrawLottery(bot))
