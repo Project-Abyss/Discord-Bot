4879e8e0be6ff3ce6afe2ea5cf6c3c703cfb6661
Nima-Huang <87530200+Nima-Huang@users.noreply.github.com>
2023-03-17 00:55:46 +0800

    fix path

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
-from cogs.greetings import Greetings
-from cogs.Poll_Function import Poll_Function
+from function.greetings import Greetings
+from function.Poll_Function import Poll_Function
 
 
 load_dotenv()
@@ -15,10 +15,10 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
-    await bot.load_extension('cogs.greetings')
-    await bot.load_extension('cogs.menu')
-    await bot.load_extension('cogs.drawlottery')
-    await bot.load_extension('cogs.Poll_Function')
+    await bot.load_extension('function.greetings')
+    await bot.load_extension('function.menu')
+    await bot.load_extension('function.drawlottery')
+    await bot.load_extension('function.Poll_Function')
 
 @bot.command(name='ping')
 async def ping(ctx):
