a0f5e1b38f4d4fbf4fd3ef69e952901e41985b46
xkeBANg <115547189+xkeBANg@users.noreply.github.com>
2023-03-18 23:00:01 +0800

    Update PollFunction_Menu.py

--- function/PollFunction_Menu.py
+++ function/PollFunction_Menu.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 class PollMenu(discord.ui.Modal, title ='VOTE'):
-    mutiOrSingle = discord.ui.TextInput(label='Mutiple Vote or Single Vote', placeholder='Muti or Sing')
+    multiOrSingle = discord.ui.TextInput(label='Multiple Vote or Single Vote', placeholder='Multi or Sing')
     pollTitle = discord.ui.TextInput(label='Poll Title', placeholder='Which color do you like?')
     timeLimit = discord.ui.TextInput(label='The End Date', placeholder='2222-02-22 22:22:22')
     optionDescription = discord.ui.TextInput(label='Option Title', placeholder='Blue Red Green......')
@@ -10,12 +10,12 @@ class PollMenu(discord.ui.Modal, title ='VOTE'):
     async def on_submit(self, interaction: discord.Interaction):
         optionGroup = str(self.optionDescription).split(" ")
         optionGroup = " ".join(str(i) for i in optionGroup)
-        if str(self.mutiOrSingle) == 'Muti':
-            await interaction.response.send_message(f'SYNTAX: !MutiAnswer {self.pollTitle} {self.timeLimit} {optionGroup}')
-        elif str(self.mutiOrSingle) == 'Sing':
+        if str(self.multiOrSingle) == 'Multi':
+            await interaction.response.send_message(f'SYNTAX: !MultiAnswer {self.pollTitle} {self.timeLimit} {optionGroup}')
+        elif str(self.multiOrSingle) == 'Sing':
             await interaction.response.send_message(f'SYNTAX: !SingleAnswer {self.pollTitle} {self.timeLimit} {optionGroup}')
         else:
-            await interaction.response.send_message("Please Choose \"Muti\" or \"Sing\" !")
+            await interaction.response.send_message("Please Choose \"Multi\" or \"Sing\" !")
 
 class PollMenuModel(commands.Cog, PollMenu):
     def __init__(self, bot):
